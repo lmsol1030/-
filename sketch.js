a54bda5e173475278c51f5e49640e7c903b6ed8a
lmsol1030 <lmsol1030@naver.com>
2014-11-11 19:16:04 +0900

    dfsdf

--- sketch.js
+++ sketch.js
@@ -1,37 +1,41 @@
 
 
- var solar, bg, earth, moon; 
+var solar, bg, earth, moon; 
  function setup() { 
      createCanvas(1000, 1000); 
    solar = loadImage("sun.png"); 
    bg = loadImage("bg.png"); 
    earth = loadImage("earth.png"); 
-   moon = loadImage("planet.png"); 
+   moon = loadImage("moon.png"); 
  } 
+ 
 
-
-  function draw() {
-
-
+ 
+ 
+ function draw() { 
+ 
+ 
   background(bg); 
-
-
-   var duration = 5000;
-   var timing = (new Date()%duration)/duration;
-   var duration2 = 6000;
-   var timing2 = (new Date()%duration2)/duration2;
-   var duration3 = 4000; 
-   var timing3 = (new Date()%duration3)/duration3; 
-   var duration4 = 3000; 
-   var timing4 = (new Date()%duration4)/duration4; 
-   
-
-
+ 
+ 
+  var duration0 = 7000; 
+  var timing0 = (new Date()%duration0)/duration0; 
+  var duration1 = 6000; 
+  var timing1 = (new Date()%duration1)/duration1; 
+  var duration2 = 5000; 
+  var timing2 = (new Date()%duration2)/duration2; 
+  var duration3 = 4000; 
+  var timing3 = (new Date()%duration3)/duration3; 
+  var duration4 = 3000; 
+  var timing4 = (new Date()%duration4)/duration4; 
+ 
+ 
   //지구라인 
   noFill(); 
-   ellipse(500,500,600,600) 
+  stroke(255); 
+  ellipse(500,500,600,600) 
  
- 
+
   //달타원라인1 
   noFill(); 
   stroke(255); 
@@ -49,14 +53,23 @@
          300, 
          150); 
  
-
-
-  //태양 
- image(solar,250,250,500,500);
-
-
  
-   //달1 
+  //달라인 
+  noFill(); 
+  stroke(255); 
+  ellipse(500 + Math.cos(timing1*2*PI)*300, 
+         500 + Math.sin(timing1*2*PI)*300, 
+         200, 
+         200); 
+ 
+ 
+  //태양 
+    image(solar,250,250,500,500); 
+ 
+ 
+ 
+ 
+    //달1 
   image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*150-100, 
     500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*70-100,200,200); 
  
@@ -71,9 +84,3 @@
  
     //지구 
   image(earth,500 + Math.cos(timing1*2*PI)*300-150,500 + Math.sin(timing1*2*PI)*300-150,300,300); 
- 
-
- } 
-
-
-
